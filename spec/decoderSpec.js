368d19762a71f8be5c7e193bb1fff4a56ad2217b
Ivan Storck <ivanoats@gmail.com>
2013-11-14 11:45:36 -0800

    failing properly

--- spec/decoderSpec.js
+++ spec/decoderSpec.js
@@ -1,7 +1,10 @@
 describe("Decoder", function() {
   it("decodes an encrypted string from the URL", function() {
-    //override the getCypherText function because we can't access document.URL directly
-    decoder.getCypherText = window.CyptoJS.AES.encrypt("a test string","password").cyphertext.toString();
+    // override the getCypherText and getPassword functions because
+    // we can't access document.URL directly
+    // and we can't ask the user for interaction
+    decoder.getCypherText = function() { decoder.cypherText = CryptoJS.AES.encrypt("a test string","password"); };
+    decoder.getPassword   = function() { decoder.password = "password"; };
     expect(decoder.decrypt()).toEqual("a test string");
   });
 });
